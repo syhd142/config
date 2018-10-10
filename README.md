ce6a0a0562d6ac7883ecaa010013ac3ad5c8910a
MOMO <momo@bj2616.local>
2018-10-10 14:20:18 +0800

    first init

---
+++ README.md
@@ -0,0 +1,33 @@
+config
+============
+
+
+我的linux下配置，一键部署，可供参考
+
+    aliases(命令别名)
+    bin(机器登陆脚本)
+    vim (一键部署强大的vim)
+    completion (常用工具命令补全)
+
+
+###NOTICE
+
+2014-12-31 更新完毕
+
+###部署
+
+一键配置，直接执行
+
+    sh -x install.sh
+    git submodule update --init
+    vj 打开Vim，输入:BundleInstal(安装vim插件)
+
+
+------------------------
+
+
+The End!
+
+Email: syhd142@gmail.com
+
+Github: https://github.com/syhd142
---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+cd .
+
+CF_DIR=`pwd`
+
+#git pull -q origin master
+
+backup()
+{
+    if [ -h "$1" ]
+    then
+        unlink "$1" 
+    elif [ -e "$1" ]
+    then
+        mv "$1" "$1".bak
+    fi
+}
+
+#vim
+backup ~/.vimrc
+backup ~/.vim
+ln -s $CF_DIR/vim/vimrc ~/.vimrc
+ln -s $CF_DIR/vim/ ~/.vim
+
+#alias
+ALIASES_DIR="$CF_DIR/aliases"
+ALIASES_FILES=`ls $ALIASES_DIR/*aliases`
+for f in $ALIASES_FILES
+do
+    F_NAME=`basename $f`
+    backup ~/"."$F_NAME
+    ln -s $f ~/"."$F_NAME
+done
+
+#completion
+COMPLETION_DIR="$CF_DIR/completions"
+ALIASES_FILES=`ls $COMPLETION_DIR/*completion`
+for f in $ALIASES_FILES
+do
+    F_NAME=`basename $f`
+    backup ~/"."$F_NAME
+    ln -s $f ~/"."$F_NAME
+done
+
+backup ~/bin
+ln -s $CF_DIR/bin ~/bin
+
+#configs
+##for ssh
+#if [ -d ~/.ssh ]
+#then
+#    mkdir -p ~/.ssh
+#fi
+#ln -s $CF_DIR/config/ssh-config ~/.ssh/config
+#
+##for git
+#ln -s $CF_DIR/config/git-config ~/.gitconfig
+
+#.bash_profile
+backup ~/.bash_profile
+ln -s $CF_DIR/aliases/bash_profile ~/.bash_profile
+. ~/.bash_profile
